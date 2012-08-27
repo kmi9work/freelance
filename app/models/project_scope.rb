ac61c23c66f68641ff4e4603824289cb0e4bec47
Michael Kostenchuk <kmi9.other@gmail.com>
2012-08-27 19:09:13 +0400

    delete files

--- app/models/project_scope.rb
+++
@@ -1,6 +0,0 @@
-class ProjectArea < ActiveRecord::Base
-  attr_accessible :project_id, :area_id
-  validates :project, :area, presence: true
-  belongs_to :project
-  belongs_to :area
-end
--- db/migrate/20120809085039_create_scopes.rb
+++
@@ -1,9 +0,0 @@
-class CreateScopes < ActiveRecord::Migration
-  def change
-    create_table :scopes do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
